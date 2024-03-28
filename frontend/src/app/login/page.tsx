07a0c247641f9814528faab7907b1edc65c9a553
Jvcentrone <joaovtcentrone@gmail.com>
2024-03-28 17:07:55 -0300

    login page implementation

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
-const page = () => {
+const LoginPage = () => {
+
   return (
-    <div>page</div>
-  )
-}
+    <div className='flex flex-col mx-auto py-32'>
+      <h1 className='mx-auto text-3xl text-custom-secondary my-8 font-bold uppercase'>Faça seu login</h1>
+      <form className='flex flex-col mx-auto'>
+        <label htmlFor="email">Usuário</label>
+        <input className='p-2 rounded-xl border-2 border-custom-secondary mb-8' type="text" id="email" />
 
-export default page
+        <label htmlFor="password">Senha</label>
+        <input className='p-2 rounded-xl border-2 border-custom-secondary mb-8' type="password" id="password" />
+
+        <button type="submit">Login</button>
+      </form>
+      <Link className='mx-auto my-8' href='/'>Voltar pra Home</Link>
+    </div>
+  );
+};
+
+export default LoginPage;
