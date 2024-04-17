481f007253a6bab546f24e87a89b8cc44feae7c1
Uendell Rios <riosuendell@outloock.com>
2024-04-16 23:37:48 -0300

    update model

--- ZventsApi/Models/User.cs
+++ ZventsApi/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ZventsApi.Models
 {
@@ -8,12 +9,17 @@ namespace ZventsApi.Models
         Vendor = 1
     }
     public class User
-    {   
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "User name is required")]
         public string UserName { get; set; }
-        public UserRole Role {  get; set; }
+        public UserRole? Role {  get; set; }
 
     }
 }
