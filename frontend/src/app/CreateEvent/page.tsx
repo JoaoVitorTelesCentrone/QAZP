acf0110aa32f0268c4464bd3d03d1c14fe38fcfc
João <joaovtcentrone@gmail.com>
2024-05-29 11:35:58 -0300

    start of the event form implementation

--- frontend/src/app/CreateEvent/page.tsx
+++ frontend/src/app/CreateEvent/page.tsx
@@ -4,6 +4,9 @@ import UserSideMenu from '../components/UserHeader'
 import axios from 'axios'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuItem } from '@radix-ui/react-dropdown-menu'
 import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { PlusCircleIcon } from 'lucide-react'
+import { materialProps } from '../Materials/page'
 
 type ClientProps = {
   id: string
@@ -12,11 +15,43 @@ type ClientProps = {
   email: string
 }
 
+type MaterialType = { 
+  category: string
+  name: string
+  price: string
+  quantity: number
+}
+
+type insertMaterialProps = {
+  name: string
+  index: number 
+
+} 
+
+const MaterialCategory =
+    [
+
+      {name: 'Comida', index: 0},
+      {name: 'Decoração', index: 1},
+      {name: 'Utensilios', index: 2},
+      {name: 'Mobilia', index: 3},
+      {name: 'Recursos humanos', index: 4},
+      {name: 'Aluguel', index: 5},
+      {name: 'Entretenimento', index: 6},
+      {name: 'Marketing', index: 7},
+    ]
+    
+
 const CreateEvent = () => {
   const [clients, setClients] = useState<ClientProps[]>([])
   const [clientEmail, setCLientEmail] = useState('')
   const [clientDocument, setCLientDocument] = useState('')
   const [clientName, setCLientName] = useState('')
+  const [selectedCategory, setSelectedCategory] = useState('')
+  const [selectedMaterial, setSelectedMaterial] = useState('')
+  const [materialQnt, setMaterialQnt] = useState(0)
+  const [materials, setMaterials] = useState<MaterialType[]>([])
+  const [insertedMaterial, setInsertedMaterial] = useState<insertMaterialProps[]>([])
 
   const getClients = async () => {
     try {
@@ -32,6 +67,21 @@ const CreateEvent = () => {
     }
   }
 
+  const getMaterialsByCategory = async (categoryName: string, category: number) => {
+    try {
+      const response = await axios.get(`http://localhost:5196/api/Material/category/${category}`)
+      console.log(response.data)
+      const materials = response.data.map((material: any) => ({
+        name: material.name, id: material.id, value: material.value
+      }))
+      setMaterials(materials)
+
+    } catch (error) {
+      console.error('Error fetching clients:', error)
+    }
+    setSelectedCategory(categoryName)
+  }
+
   useEffect(() => {
     getClients()
   }, [])
@@ -41,54 +91,78 @@ const CreateEvent = () => {
     setCLientEmail(email)
     setCLientDocument(documentId)
     setCLientName(name)
-    clients.splice(index)
+  }
+
+  const insertMaterial = (index:number) => { 
+    materials.splice(index)
   }
   return (
     <div>
       <UserSideMenu />
       <div className=''>
         <h1 className='text-4xl font-bold mx-32 my-8'>Criar evento</h1>
-        <form action="" className='flex flex-col justify-around w-[800px] bg-slate-400 p-16 mx-auto'>
-          <div className='flex'>
-
-          <DropdownMenu>
-            <DropdownMenuTrigger>Selecione um cliente</DropdownMenuTrigger>
-            <DropdownMenuContent>
-              {clients.map((client, index) => (
-                <DropdownMenuItem onClick={() => getCLienInputValues(client.email, client.documentId, client.name, index)} key={index}>{client.name}</DropdownMenuItem>
-              ))}
-            </DropdownMenuContent>
-          </DropdownMenu>
-
-          <div className="flex flex-col">
-            <h1 className='p-2'>Nome</h1>
-            <h1  className='p-2 border-2 rounded-xl'>{clientName}</h1>
-          </div>
-
-          <div className="flex flex-col">
-            <h1 className='p-2'>Email</h1>
-            <h1 className='p-2 border-2 rounded-xl'>{clientEmail}</h1>
-          </div>
-
-          <div className="flex flex-col">
-            <h1 className='p-2'>Documento</h1>
-            <h1  className='p-2 border-2 rounded-xl'>{clientDocument}</h1>
-          </div>
-          </div>
-          <h1 className="font-bold">Informações do cliente</h1>
-          <div className='my-4 flex-col'>
-            <Input /> 
-            <Input /> 
-            <Input /> 
-            <Input /> 
-            <Input /> 
-          </div>
-          <h1 className="font-bold">Materiais</h1>
-          <div className='flex my-4'>
+        <form action="" className='flex flex-col  w-[800px] rounded-xl bg-opacity-30 bg-slate-400 p-16 mx-auto'>
+          <h1 className="font-bold text-2xl">Informações do cliente</h1>
+          <div className='flex justify-around my-4'>
+            <DropdownMenu>
+              <DropdownMenuTrigger className='border-[0.2px] w-[200px] h-[50px] mt-4 bg-white rounded-xl mr-4 font-bold'>{clientName? clientName : 'Selecione um cliente'}</DropdownMenuTrigger>
+              <DropdownMenuContent className='bg-white p-2 border-2 rounded-xl mr-72 '>
+                {clients.map((client, index) => (
+                  <>
+                    <DropdownMenuItem onClick={() => getCLienInputValues(client.email, client.documentId, client.name, index)} key={index}>{client.name}</DropdownMenuItem>
+                    <hr  className='h-2'/>
+                  </>
+                  
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
             <div className="flex justify-around">
-              <Input className='mx-8' /> 
-              <Input className='mx-8' /> 
-              <Input /> 
+
+              <div className="mx-2">
+                <h1 className='p-1'>Email</h1>
+                <h1 className='w-[180px] bg-white text-center border-2 h-[33px] rounded-xl'>{clientEmail}</h1>
+              </div>
+
+              <div className="mx-2">
+                <h1 className='p-1'>Documento</h1>
+                <h1  className='w-[180px] bg-white text-center border-2 h-[33px] rounded-xl'>{clientDocument}</h1>
+              </div>
+            </div>
+          </div>
+          <h1 className="font-bold text-2xl my-1">Informações do Evento</h1>
+          <div className='my-4 flex justify-around'>
+            <Input className='bg-white text-black mx-2' placeholder='Digite o Estado'/> 
+            <Input className='bg-white text-black mx-2' placeholder='Digite a Cidade'/> 
+            <Input className='bg-white text-black mx-2' placeholder='Digite a Rua'/> 
+          </div>
+          <h1 className="font-bold text-xl my-1">Selecione os Materiais</h1>
+          <div className='flex my-4'>
+            <div className="flex justify-around w-full">
+            <DropdownMenu>
+              <DropdownMenuTrigger className='border-[0.2px] h-[50px] mt-4 mx-2 w-[30%] bg-white rounded-xl mr-4 font-bold'>{selectedCategory ? selectedCategory : 'Categorias'}</DropdownMenuTrigger>
+              <DropdownMenuContent className='bg-white p-2 border-2 rounded-xl mr-72 '>
+                {MaterialCategory.map((category, index) => (
+                  <>
+                    <DropdownMenuItem onClick={() => getMaterialsByCategory(category.name, index)} key={index}>{category.name}</DropdownMenuItem>
+                    <hr  className='h-2'/>
+                  </>      
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger className='border-[0.2px] h-[50px] mt-4 mx-2 w-[40%] bg-white rounded-xl mr-4 font-bold'>{selectedMaterial ? selectedMaterial:'Materiais'}</DropdownMenuTrigger>
+              <DropdownMenuContent className='bg-white p-2 border-2 rounded-xl mt-2 '>
+                {materials.map((material, index) => (
+                  <div key={index}>
+                    <DropdownMenuItem onClick={() => setSelectedMaterial(material.name)}  key={index}>{material.name}</DropdownMenuItem>
+                    <hr  className='h-2'/>
+                  </div>      
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+              <Input onChange={(e) => setMaterialQnt(e.target.valueAsNumber)} type='number' className='bg-white w-[20%] h-[50px] rounded-xl mt-4 mx-2' placeholder='Quantidade' /> 
+              <Button  className='w-[10%] mx-2 mt-4 h-[50px] rounded-full bg-white'><PlusCircleIcon className='h-8 w-8' /></Button> 
             </div>
           </div>
         </form>
