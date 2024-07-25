a5e0999aeba5841184ad8154c7d61daccfc78157
Joao <joaovtcentrone@gmail.com>
2024-07-25 18:40:12 -0300

    implement loading in all pages

--- frontend/src/app/Users/page.tsx
+++ frontend/src/app/Users/page.tsx
@@ -8,43 +8,59 @@ import { DataTable } from '@/components/ui/data-table'
 import { UsersTable } from './UsersTable'
 import axios from 'axios'
 import { ColumnDef } from '@tanstack/react-table'
+import ClipLoader from 'react-spinners/ClipLoader'
 
 const Users = () => {
   const [userData, setUserData] = useState([])
+  const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    async function fetchUserData() {
-      try {
-        const response = await axios.get('http://localhost:5196/api/User')
+  async function fetchUserData() {
+    try {
+      const response = await axios.get('http://localhost:5196/api/User')
 
-        setUserData(response.data)
-      } catch (error) {
-        console.error('Erro ao fazer a requisição:', error)
-        throw error
-      }
+      setUserData(response.data)
+    } catch (error) {
+      console.error('Erro ao fazer a requisição:', error)
+      throw error
     }
-    fetchUserData()
+  }
+  useEffect(() => {
+    const fetch = async () => {
+      setLoading(true)
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      await Promise.all([fetchUserData()])
+      setLoading(false)
+    }
+    fetch()
   }, [])
 
   const columns = useMemo(() => userColumns(), [])
   return (
     <div className="flex flex-col">
-      <UserHeader />
-      <div className="flex my-20 mx-10 justify-between">
-        <h1 className="text-4xl ml-72 font-bold uppercase text-secondary-foreground">
-          Usuários
-        </h1>
-        <Link
-          className="flex bg-primary hover:transition delay-100 hover:scale-125 p-4 rounded-xl text-white"
-          href="/CreateUser"
-        >
-          Criar usuário
-          <Plus className="h-4 w-4 mt-1 ml-2" />{' '}
-        </Link>
-      </div>
-      <div className="mx-auto h-screen">
-        <UsersTable data={userData} columns={columns} />
-      </div>
+      {loading ? (
+        <div className="flex justify-center items-center h-screen mx-auto">
+          <ClipLoader size={50} color={'#123abc'} loading={loading} />
+        </div>
+      ) : (
+        <>
+          <UserHeader />
+          <div className="flex my-20 mx-10 justify-between">
+            <h1 className="text-4xl ml-72 font-bold uppercase text-secondary-foreground">
+              Usuários
+            </h1>
+            <Link
+              className="flex bg-primary hover:transition delay-100 hover:scale-125 p-4 rounded-xl text-white"
+              href="/CreateUser"
+            >
+              Criar usuário
+              <Plus className="h-4 w-4 mt-1 ml-2" />{' '}
+            </Link>
+          </div>
+          <div className="mx-auto h-screen">
+            <UsersTable data={userData} columns={columns} />
+          </div>
+        </>
+      )}
     </div>
   )
 }
