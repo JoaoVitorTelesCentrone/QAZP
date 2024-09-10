0de6d44528901a2b7e1ba7e729385c8bc512bfd4
Jvcentrone <joaovtcentrone@gmail.com>
2024-09-09 21:10:31 -0300

    create client modal implementation

--- frontend/src/app/clients/page.tsx
+++ frontend/src/app/clients/page.tsx
@@ -16,13 +16,15 @@ import { Button, Tooltip } from 'antd'
 import UserSideMenu from '../components/UserHeader'
 import { FaUserPlus } from 'react-icons/fa'
 import { FaUsers } from "react-icons/fa6";
-
+import CreateClientForm from '../CreateClient/page'
+import CreateClientModal from './CreateClientModal'
 
 const Clients = () => {
   const [isLogged, setIsLogged] = useAtom(authAtom)
   const [clients, setClients] = useState([])
   const [loading, setLoading] = useState(false)
   const [clientChange, setClientChange] = useAtom(clientChangeAtom)
+  const [openModal, setOpenModal] = useState(false)
 
   useEffect(() => {
     fetchUserData()
@@ -53,43 +55,48 @@ const Clients = () => {
   }
   return (
     <div>
+      {openModal && (
+        <CreateClientModal
+          isVisible={openModal}
+          onClose={() => setOpenModal(false)}
+        />
+      )}
       {loading ? (
         <div className="flex justify-center items-center h-screen">
           <ClipLoader size={50} color={'#123abc'} loading={loading} />
         </div>
       ) : (
         <>
-        <UserSideMenu />
-        <div className="bg-tertiary h-screen">
-          <div className="p-10 ">
-            <div className="flex mt-4 justify-between w-full">
-              <div className="flex ml-64">
-                <FaUsers className=" w-16 h-16 p-1 rounded-full my-5 text-primary border-2 border-primary" />
+          <UserSideMenu />
+          <div className="bg-tertiary h-screen">
+            <div className="p-10 ">
+              <div className="flex mt-4 justify-between w-full">
+                <div className="flex ml-64">
+                  <FaUsers className=" w-16 h-16 p-1 rounded-full my-5 text-primary border-2 border-primary" />
 
-                <h1 className="font-monospace font-semibold text-primary text-7xl my-4 mx-4 text-secondary-foreground">
-                  Clientes
-                </h1>
+                  <h1 className="font-monospace font-semibold text-primary text-7xl my-4 mx-4 text-secondary-foreground">
+                    Clientes
+                  </h1>
+                </div>
+                <Button
+                  icon={<FaUserPlus className="w-5 h-5 " />}
+                  type="primary"
+                  className="mt-8"
+                  size="large"
+                  onClick={() => setOpenModal(true)}
+                >
+                  <h1 className="text-lg">Criar novo cliente</h1>
+                </Button>
               </div>
-              <Button
-                icon={<FaUserPlus className="w-5 h-5 " />}
-                type="primary"
-                className="mt-8"
-                size="large"
-              >
-                <Link href="/CreateClient" className="text-lg">
-                  Criar novo cliente
-                </Link>
-              </Button>
+            </div>
+            <div className="ml-72 mr-10">
+              <ClientTable columns={clientColumns} data={clients} />
             </div>
           </div>
-          <div className="ml-72 mr-10">
-            <ClientTable columns={clientColumns} data={clients} />
-          </div>
-        </div>
-      </>
-    )}
-  </div>
-)
+        </>
+      )}
+    </div>
+  )
 }
 
 export default Clients
