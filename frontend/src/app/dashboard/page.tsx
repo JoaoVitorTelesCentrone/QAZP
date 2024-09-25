3fc1c5505bc4215e08cf955cf507f5fa52590f97
Gustavo Alves <gustavoalvesvaladao@gmail.com>
2024-09-25 00:32:57 -0300

    dashboard optimization

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -7,62 +7,61 @@ import { useAtom } from 'jotai'
 import axios from 'axios'
 import { Events, eventsColumns } from './columns'
 import { DashboardTable } from './DashboardTable'
-import { LucideLineChart, Calendar } from 'lucide-react'
+import { LucideLineChart } from 'lucide-react'
 import { userInfoAtom } from '../atoms/userInfoAtom'
 import ClipLoader from 'react-spinners/ClipLoader'
 import UserSideMenu from '../components/UserHeader'
-import { MdEventAvailable } from "react-icons/md";
-
+import { MdEventAvailable } from "react-icons/md"
 
 const Dashboard = () => {
   const [isLogged] = useAtom(authAtom)
   const [loading, setLoading] = useState(true)
   const [user] = useAtom(userInfoAtom)
-  const [numberOfClients, setNumberOfClients] = useState('')
-  const [numberOfUsers, setNumberOfUsers] = useState('')
-  const [numberOfEvents, setNumberOfEvents] = useState('')
+  const [counts, setCounts] = useState({
+    clients: '0',
+    users: '0',
+    events: '0'
+  })
   const [events, setEvents] = useState<Events[]>([])
 
   if (!isLogged) {
     redirect('/login')
   }
 
-  const getClients = async () => {
+  const fetchData = async (url: string) => {
     try {
-      const response = await axios.get('http://localhost:5196/api/Client')
-      setNumberOfClients(response.data.length.toString())
+      const response = await axios.get(url)
+      return response.data
     } catch (error) {
-      console.error('Error fetching clients:', error)
+      console.error(`Error fetching data from ${url}:`, error)
+      return []
     }
+  }
+
+  const getClients = async () => {
+    const clients = await fetchData('http://localhost:5196/api/Client')
+    setCounts(prevCounts => ({ ...prevCounts, clients: clients.length.toString() }))
   }
 
   const getUsers = async () => {
-    try {
-      const response = await axios.get('http://localhost:5196/api/User')
-      setNumberOfUsers(response.data.length.toString())
-    } catch (error) {
-      console.error('Error fetching users:', error)
-    }
+    const users = await fetchData('http://localhost:5196/api/User')
+    setCounts(prevCounts => ({ ...prevCounts, users: users.length.toString() }))
   }
 
   const getEvents = async () => {
-    try {
-      const response = await axios.get('http://localhost:5196/api/Event')
-      setNumberOfEvents(response.data.length.toString())
-      setEvents(response.data)
-    } catch (error) {
-      console.error('Error fetching events:', error)
-    }
+    const eventsData = await fetchData('http://localhost:5196/api/Event')
+    setCounts(prevCounts => ({ ...prevCounts, events: eventsData.length.toString() }))
+    setEvents(eventsData)
   }
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchDataFromAPIs = async () => {
       setLoading(true)
       await Promise.all([getClients(), getUsers(), getEvents()])
       setLoading(false)
     }
 
-    fetchData()
+    fetchDataFromAPIs()
   }, [])
 
   return (
@@ -72,57 +71,56 @@ const Dashboard = () => {
           <ClipLoader size={50} color={'#123abc'} loading={loading} />
         </div>
       ) : (
-            <>
-              <UserSideMenu />
-              <div className="bg-tertiary h-screen">                
-              <div className="flex ml-72">
-                <LucideLineChart className="w-16 h-16 p-1 rounded-full my-12 mx-2 text-primary border-2 border-primary" />
-              <h1 className="font-monospace font-semibold text-6xl my-12  text-secondary-foreground">
+        <>
+          <UserSideMenu />
+          <div className="bg-tertiary h-screen">
+            <div className="flex ml-72">
+              <LucideLineChart className="w-16 h-16 p-1 rounded-full my-12 mx-2 text-primary border-2 border-primary" />
+              <h1 className="font-monospace font-semibold text-6xl my-12 text-secondary-foreground">
                 Dashboards operacionais
               </h1>
-              </div>              
-              <div className="flex mx-24">
-                <div className="flex p-1 mx-auto ml-72 my-2 w-[1100px]">
-                  <div className="rounded-xl bg-gray-700 bg-opacity-10 border-2 border-secondary p-8">
-                    <h1 className="text-3xl text-gray-400 font-bold">
-                      Número de Clientes
-                    </h1>
-                    <h1 className="text-6xl text-gray-400 font-extrabold uppercase">
-                      {numberOfClients}
-                    </h1>
-                  </div>
-                  <div className="max-xl:mb-10 max-xl:mx-0 rounded-xl mx-4 border-2 bg-gray-700 bg-opacity-10 border-secondary p-8">
-                    <h1 className="text-3xl font-bold text-gray-400">
-                      Número de Usuários
-                    </h1>
-                    <h1 className="text-6xl text-gray-400 font-extrabold uppercase">
-                      {numberOfUsers}
-                    </h1>
-                  </div>
-
-                  <div className="max-xl:mb-10 rounded-xl border-2 bg-gray-700 bg-opacity-10 border-secondary p-8">
-                    <h1 className="text-3xl text-gray-400 font-bold">
-                      Número de Eventos
-                    </h1>
-                    <h1 className="text-6xl text-gray-400 font-extrabold uppercase">
-                      {numberOfEvents}
-                    </h1>
-                  </div>
-                </div>                
+            </div>
+            <div className="flex mx-24">
+              <div className="flex p-1 mx-auto ml-72 my-2 w-[1100px]">
+                <div className="rounded-xl bg-gray-700 bg-opacity-10 border-2 border-secondary p-8">
+                  <h1 className="text-3xl text-gray-400 font-bold">
+                    Número de Clientes
+                  </h1>
+                  <h1 className="text-6xl text-gray-400 font-extrabold uppercase">
+                    {counts.clients}
+                  </h1>
+                </div>
+                <div className="max-xl:mb-10 max-xl:mx-0 rounded-xl mx-4 border-2 bg-gray-700 bg-opacity-10 border-secondary p-8">
+                  <h1 className="text-3xl font-bold text-gray-400">
+                    Número de Usuários
+                  </h1>
+                  <h1 className="text-6xl text-gray-400 font-extrabold uppercase">
+                    {counts.users}
+                  </h1>
+                </div>
+                <div className="max-xl:mb-10 rounded-xl border-2 bg-gray-700 bg-opacity-10 border-secondary p-8">
+                  <h1 className="text-3xl text-gray-400 font-bold">
+                    Número de Eventos
+                  </h1>
+                  <h1 className="text-6xl text-gray-400 font-extrabold uppercase">
+                    {counts.events}
+                  </h1>
+                </div>
               </div>
-              <div className="flex ml-72">
-                <MdEventAvailable  className=" w-10 h-10 p-1 rounded-full my-9 mx-2 text-primary border-2 border-primary" />
-                <h1 className="font-monospace font-bold text-5xl my-8 text-secondary-foreground">
+            </div>
+            <div className="flex ml-72">
+              <MdEventAvailable className="w-10 h-10 p-1 rounded-full my-9 mx-2 text-primary border-2 border-primary" />
+              <h1 className="font-monospace font-bold text-5xl my-8 text-secondary-foreground">
                 Próximos eventos
               </h1>
-              </div>
-              <div className="ml-72 mr-10">
-                <DashboardTable columns={eventsColumns} data={events} />
-              </div>
-              </div>
-            </>
-          )}
-        </div>
+            </div>
+            <div className="ml-72 mr-10">
+              <DashboardTable columns={eventsColumns} data={events} />
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   )
 }
 
