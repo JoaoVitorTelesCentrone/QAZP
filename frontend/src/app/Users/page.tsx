82820cdddc8e21e3075526e656e2be8a7f84b7dc
Gustavo Alves <gustavoalvesvaladao@gmail.com>
2024-09-25 00:35:29 -0300

    optimizing users

--- frontend/src/app/Users/page.tsx
+++ frontend/src/app/Users/page.tsx
@@ -12,36 +12,29 @@ import { useAtom } from 'jotai'
 import CreateUserModal from './createUserModal'
 
 const Users = () => {
-  const [userData, setUserData] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [change, setChange] = useAtom(userChangeAtom)
+  const [userData, setUserData] = useState<Users[]>([])
+  const [loading, setLoading] = useState(true)
+  const [change] = useAtom(userChangeAtom)
   const [openModal, setOpenModal] = useState(false)
 
-  useEffect(() => {
-    fetchUserData()
-  }, [change])
-
-  async function fetchUserData() {
+  const fetchUserData = useCallback(async () => {
+    setLoading(true)
     try {
       const response = await axios.get('http://localhost:5196/api/User')
-
       setUserData(response.data)
     } catch (error) {
       console.error('Erro ao fazer a requisição:', error)
-      throw error
-    }
-  }
-  useEffect(() => {
-    const fetch = async () => {
-      setLoading(true)
-      await new Promise(resolve => setTimeout(resolve, 1000))
-      await Promise.all([fetchUserData()])
+    } finally {
       setLoading(false)
     }
-    fetch()
   }, [])
 
+  useEffect(() => {
+    fetchUserData()
+  }, [fetchUserData, change])
+
   const columns = useMemo(() => userColumns(), [])
+
   return (
     <div>
       {openModal && (
@@ -58,17 +51,16 @@ const Users = () => {
         <>
           <UserSideMenu />
           <div className="bg-tertiary h-screen">
-            <div className="p-10 ">
+            <div className="p-10">
               <div className="flex mt-4 justify-between w-full">
                 <div className="flex ml-64">
-                  <FaUser className=" w-16 h-16 p-1 rounded-full my-5 text-primary border-2 border-primary" />
-
+                  <FaUser className="w-16 h-16 p-1 rounded-full my-5 text-primary border-2 border-primary" />
                   <h1 className="font-monospace font-semibold text-7xl my-3 mx-4 text-secondary-foreground">
                     Usuários
                   </h1>
                 </div>
                 <Button
-                  icon={<FaUserPlus className="w-5 h-5 " />}
+                  icon={<FaUserPlus className="w-5 h-5" />}
                   type="primary"
                   className="mt-8"
                   size="large"
