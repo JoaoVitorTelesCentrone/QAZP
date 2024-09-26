d17b00d34c3045d8795d556949d443fa55b890a5
Gustavo Alves <gustavoalvesvaladao@gmail.com>
2024-09-25 22:29:13 -0300

    quote adjust

--- frontend/src/app/quote/page.tsx
+++ frontend/src/app/quote/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import UserSideMenu from '../components/UserHeader'
 import { useAtom } from 'jotai'
 import { authAtom } from '../atoms/authAtom'
@@ -9,64 +9,56 @@ import { QuoteTable } from './QuoteTable'
 import axios from 'axios'
 import ClipLoader from 'react-spinners/ClipLoader'
 import { quoteChangeAtom } from '../atoms/changeQuoteAtom'
-import { GiTakeMyMoney } from "react-icons/gi";
-
+import { GiTakeMyMoney } from "react-icons/gi"
 
 const Page = () => {
-  const [auth, isAuth] = useAtom(authAtom)
+  const [auth] = useAtom(authAtom)
   const [quote, setQuote] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [quoteChange, setQuoteChange] = useAtom(quoteChangeAtom)
+  const [loading, setLoading] = useState(true)
+  const [quoteChange] = useAtom(quoteChangeAtom)
 
-  useEffect(() => {
-    fetchUserData()
-  }, [quoteChange])
-
+  // Redireciona se o usuário não estiver autenticado
   if (!auth) {
     redirect('/login')
   }
-  async function fetchUserData() {
+
+  const fetchUserData = useCallback(async () => {
+    setLoading(true)
     try {
       const response = await axios.get('http://localhost:5196/api/Quote')
-
       setQuote(response.data)
     } catch (error) {
       console.error('Erro ao fazer a requisição:', error)
-      throw error
-    }
-  }
-
-  useEffect(() => {
-    const fetch = async () => {
-      setLoading(true)
-      await new Promise(resolve => setTimeout(resolve, 500))
-      await Promise.all([fetchUserData()])
+    } finally {
       setLoading(false)
     }
-    fetch()
   }, [])
+
+  useEffect(() => {
+    fetchUserData()
+  }, [fetchUserData, quoteChange])
+
   return (
-<div>
+    <div>
       {loading ? (
         <div className="flex justify-center items-center h-screen">
           <ClipLoader size={50} color={'#123abc'} loading={loading} />
         </div>
       ) : (
         <>
-        <UserSideMenu />
-        <div className="bg-tertiary h-screen">
-          <div className="p-10 ">
-            <div className="flex mt-4 justify-between w-full">
-              <div className="flex ml-64">
-                <GiTakeMyMoney  className=" w-16 h-16 p-1 rounded-full my-5 text-primary border-2 border-primary" />
-
-                <h1 className="font-monospace font-semibold text-7xl my-3 mx-4 text-secondary-foreground text-primary">
-                  Orçamentos
-                </h1>
+          <UserSideMenu />
+          <div className="bg-tertiary h-screen">
+            <div className="p-10">
+              <div className="flex mt-4 justify-between w-full">
+                <div className="flex ml-64">
+                  <GiTakeMyMoney className="w-16 h-16 p-1 rounded-full my-5 text-primary border-2 border-primary" />
+                  <h1 className="font-monospace font-semibold text-7xl my-3 mx-4 text-secondary-foreground text-primary">
+                    Orçamentos
+                  </h1>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="ml-72 mr-10">
+            <div className="ml-72 mr-10">
               <QuoteTable columns={quoteColumns} data={quote} />
             </div>
           </div>
