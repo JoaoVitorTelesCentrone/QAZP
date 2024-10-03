49f5f383f52bd8e3312764fe30ca0b0f432013f4
Uendell Rios <uendell.rios@zbra.dev>
2024-10-02 23:49:34 -0300

    improve events datagrid

--- frontend/src/functions/functions.ts
+++ frontend/src/functions/functions.ts
@@ -164,3 +164,8 @@ export function documentIdConverter(documentId: string) {
   }
   return formatedDocumentId
 }
+
+export function formatDate(dateString: string): string {
+  const [year, month, day] = dateString.split('-');
+  return `${day}/${month}/${year}`;
+}
