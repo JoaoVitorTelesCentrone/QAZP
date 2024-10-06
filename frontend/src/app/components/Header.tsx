0c13d5d6e94362c203abc0fa23b611ddaf465517
Gustavo Alves <gustavoalvesvaladao@gmail.com>
2024-10-06 14:09:36 -0300

    removing loginModal

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -5,14 +5,11 @@ import React, { useState } from 'react'
 import { authAtom } from '../atoms/authAtom'
 import { userInfoAtom } from '../atoms/userInfoAtom'
 import UserHeader from './UserHeader'
-import { Button } from 'antd' 
-import LoginModal from './LoginModal'
 
 const Header = () => {
   const [isLogged, setIsLogged] = useAtom(authAtom)
   const [user, setUser] = useAtom(userInfoAtom)
-  const [isModalVisible, setIsModalVisible] = useState(false)
-
+  
   return (
     <div className=" flex p-8 bg-quartenary text-secondary justify-around">
       <Link
@@ -25,12 +22,6 @@ const Header = () => {
         <UserHeader />
       ) : (
         <>
-          {isModalVisible && (
-            <LoginModal
-              isModalVisible={isModalVisible}
-              setIsModalVisible={setIsModalVisible}
-            />
-          )}
           <ul className="flex justify-center mx-auto">
             <Link
               href="/"
