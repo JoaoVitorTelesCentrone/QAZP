68be2107efe53c3d517e272a1e7c9a09e78d0ba2
Uendell Rios <riosuendell@outloock.com>
2025-04-14 10:09:31 -0300

    modulo 1

---
+++ frontend/cypress/e2e/login/login.spec.ts
@@ -0,0 +1,14 @@
+describe('Login', () => {
+  it('Should Login on QAZP successfully', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('.p-8 > .ant-btn').click()
+
+    cy.get('[id="username"]').type('admin')
+    cy.get('[id="password"]').type('123')
+    cy.get('[data-testid="login-button"]').click()
+
+    cy.get('[class="ant-modal-title"]').should('not.exist')
+    cy.get('[data-content=""] > div').should('be.visible').should('have.text', 'Bem-vindo, Administrador!')
+    cy.url().should('equal', 'http://localhost:3000/dashboard')
+  })
+})
