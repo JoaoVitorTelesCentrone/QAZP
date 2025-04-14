479c318e307b5c3c13e7dcaea21d2b42ca0c1db3
Gustavo Alves <gustavoalvesvaladao@gmail.com>
2025-04-14 10:51:06 -0300

    creating tests

--- frontend/cypress/e2e/login/login.spec.ts
+++ frontend/cypress/e2e/login/login.spec.ts
@@ -11,4 +11,43 @@ describe('Login', () => {
     cy.get('[data-content=""] > div').should('be.visible').should('have.text', 'Bem-vindo, Administrador!')
     cy.url().should('equal', 'http://localhost:3000/dashboard')
   })
+  it('Should visit Orçamentos Page', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('.p-8 > .ant-btn').click()
+
+    cy.get('[id="username"]').type('admin')
+    cy.get('[id="password"]').type('123')
+    cy.get('[data-testid="login-button"]').click()
+
+    cy.get('[class="ant-modal-title"]').should('not.exist')
+    cy.get('[data-content=""] > div').should('be.visible').should('have.text', 'Bem-vindo, Administrador!')
+    cy.url().should('equal', 'http://localhost:3000/dashboard')
+    cy.get(':nth-child(2) > .block').click();
+    cy.url().should('equal', 'http://localhost:3000/quote')
+  })
+  it('Should visit Clientes Page', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('.p-8 > .ant-btn').click()
+
+    cy.get('[id="username"]').type('admin')
+    cy.get('[id="password"]').type('123')
+    cy.get('[data-testid="login-button"]').click()
+
+    cy.get('[class="ant-modal-title"]').should('not.exist')
+    cy.get('[data-content=""] > div').should('be.visible').should('have.text', 'Bem-vindo, Administrador!')
+    cy.url().should('equal', 'http://localhost:3000/dashboard')
+    cy.get(':nth-child(3) > .block').click();
+    cy.url().should('equal', 'http://localhost:3000/clients')
+  })
+  it('Should logout the user', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('.p-8 > .ant-btn').click()
+
+    cy.get('[id="username"]').type('admin')
+    cy.get('[id="password"]').type('123')
+    cy.get('[data-testid="login-button"]').click()
+    cy.get('.fixed > .text-white').click();
+
+    cy.url().should('equal', 'http://localhost:3000/');
+  })
 })
