5373b76a2efb3798d7a0e717084337777d1a8a52
Gustavo Alves <gustavoalvesvaladao@gmail.com>
2025-04-19 14:22:47 -0300

    creating a beforeEach and afterEach

--- frontend/cypress/e2e/login/login.spec.ts
+++ frontend/cypress/e2e/login/login.spec.ts
@@ -1,53 +1,25 @@
 describe('Login', () => {
-  it('Should Login on QAZP successfully', () => {
+
+  const fazerLogin = () => {
     cy.visit('http://localhost:3000/')
     cy.get('.p-8 > .ant-btn').click()
-
     cy.get('[id="username"]').type('admin')
     cy.get('[id="password"]').type('123')
     cy.get('[data-testid="login-button"]').click()
+  }
+beforeEach(() =>{
+  fazerLogin()
+})
 
-    cy.get('[class="ant-modal-title"]').should('not.exist')
-    cy.get('[data-content=""] > div').should('be.visible').should('have.text', 'Bem-vindo, Administrador!')
-    cy.url().should('equal', 'http://localhost:3000/dashboard')
-  })
+afterEach(() =>{
+  cy.get('.fixed > .text-white').click();
+})
   it('Should visit Orçamentos Page', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('.p-8 > .ant-btn').click()
-
-    cy.get('[id="username"]').type('admin')
-    cy.get('[id="password"]').type('123')
-    cy.get('[data-testid="login-button"]').click()
-
-    cy.get('[class="ant-modal-title"]').should('not.exist')
-    cy.get('[data-content=""] > div').should('be.visible').should('have.text', 'Bem-vindo, Administrador!')
-    cy.url().should('equal', 'http://localhost:3000/dashboard')
     cy.get(':nth-child(2) > .block').click();
     cy.url().should('equal', 'http://localhost:3000/quote')
   })
   it('Should visit Clientes Page', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('.p-8 > .ant-btn').click()
-
-    cy.get('[id="username"]').type('admin')
-    cy.get('[id="password"]').type('123')
-    cy.get('[data-testid="login-button"]').click()
-
-    cy.get('[class="ant-modal-title"]').should('not.exist')
-    cy.get('[data-content=""] > div').should('be.visible').should('have.text', 'Bem-vindo, Administrador!')
-    cy.url().should('equal', 'http://localhost:3000/dashboard')
     cy.get(':nth-child(3) > .block').click();
     cy.url().should('equal', 'http://localhost:3000/clients')
-  })
-  it('Should logout the user', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('.p-8 > .ant-btn').click()
-
-    cy.get('[id="username"]').type('admin')
-    cy.get('[id="password"]').type('123')
-    cy.get('[data-testid="login-button"]').click()
-    cy.get('.fixed > .text-white').click();
-
-    cy.url().should('equal', 'http://localhost:3000/');
   })
 })
